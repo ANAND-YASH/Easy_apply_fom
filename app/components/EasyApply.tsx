6d1eb6f5adc1c9cec7e29490212f3258aa3a765a
ANAND-YASH <115371364+ANAND-YASH@users.noreply.github.com>
2025-04-14 10:07:41 +0530

    name change here

--- app/components/EasyApply.tsx
+++ app/components/EasyApply.tsx
@@ -1,4 +1,4 @@
-// components/EasyApply.tsx
+
 "use client";
 import React, { useState, useEffect } from 'react';
 
--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -6,17 +6,17 @@ const TopBar = () => {
     <div className="flex justify-between items-center p-4 border-b">
       <div className="flex items-center gap-2">
         <Image 
-          src="/image1.png" 
-          alt="Huzzle ltd " 
+          src="/imagess1.png" 
+          alt="comapny  " 
           width={40} 
           height={40} 
         />
-        <span className="font-semibold text-sm">Huzzle ltd </span>
+        <span className="font-semibold text-sm">Any company </span>
       </div>
 
       <div className="text-right">
         <h1 className="text-base font-bold">Frontend  Developer</h1>
-        <p className="text-sm text-gray-600">📍 London, UK</p>
+        <p className="text-sm text-gray-600">📍 Europe</p>
       </div>
     </div>
   );
